4c87bd89f80693cca4beac223e8255293eb670c4
Sascha Lisson <sl@q60.de>
2025-07-02 09:20:43 +0200

    feat: draft branch can be merged with changes from git

--- src/pages/connectivity/git/DraftsTable.tsx
+++ src/pages/connectivity/git/DraftsTable.tsx
@@ -5,19 +5,33 @@ import TableHead from "@mui/material/TableHead";
 import TableBody from "@mui/material/TableBody";
 import {
   DraftConfig,
+  GitBranchStatusData,
+  useGetDraftRebaseJobQuery,
+  useListBranchesQuery,
   useListDraftsInRepositoryQuery,
+  useRebaseDraftMutation,
 } from "../../../api/gitConnectorApi.ts";
 import IconButton from "@mui/material/IconButton";
-import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import MergeIcon from "@mui/icons-material/Merge";
+import Tooltip from "@mui/material/Tooltip";
 
 export function DraftsTable({ repositoryId }: { repositoryId: string }) {
   const draftsQuery = useListDraftsInRepositoryQuery(
     { repositoryId: repositoryId },
     { pollingInterval: 3000 },
   );
+  const branchesQuery = useListBranchesQuery(
+    { repositoryId: repositoryId },
+    { pollingInterval: 5000 },
+  );
 
   if (draftsQuery.isLoading) return "Loading...";
   if (draftsQuery.isError) return "Error: " + JSON.stringify(draftsQuery.error);
+
+  const branchStatusByName = new Map<string, GitBranchStatusData>();
+  branchesQuery.data?.branches.forEach((branch) =>
+    branchStatusByName.set(branch.name, branch),
+  );
 
   return (
     <Table>
@@ -25,22 +39,72 @@ export function DraftsTable({ repositoryId }: { repositoryId: string }) {
         <TableRow>
           <TableCell>ID</TableCell>
           <TableCell>Branch</TableCell>
+          <TableCell>Git Base Commit</TableCell>
+          <TableCell align="right">Actions</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
         {(draftsQuery.data?.drafts ?? []).map((draft) => (
-          <DraftRow key={draft.id} draft={draft} />
+          <DraftRow
+            key={draft.id}
+            draft={draft}
+            branchStatus={branchStatusByName.get(draft.gitBranchName)}
+          />
         ))}
       </TableBody>
     </Table>
   );
 }
 
-function DraftRow({ draft }: { draft: DraftConfig }) {
+function DraftRow({
+  draft,
+  branchStatus,
+}: {
+  draft: DraftConfig;
+  branchStatus?: GitBranchStatusData;
+}) {
+  const [rebaseDraftMutation, rebaseDraftMutationResult] =
+    useRebaseDraftMutation();
+  const rebaseJobQuery = useGetDraftRebaseJobQuery(
+    { draftId: draft.id },
+    { pollingInterval: 3000 },
+  );
+  const canRebase = draft.baseGitCommit !== branchStatus?.gitCommitHash;
+  const rebaseActive =
+    rebaseJobQuery.isLoading || rebaseJobQuery.data?.active === true;
+
   return (
     <TableRow>
       <TableCell>{draft.id}</TableCell>
       <TableCell>{draft.gitBranchName}</TableCell>
+      <TableCell>{draft.baseGitCommit}</TableCell>
+      <TableCell align="right">
+        {
+          <Tooltip
+            title={
+              canRebase
+                ? rebaseDraftMutationResult.isLoading
+                  ? "Rebasing..."
+                  : `Rebase onto ${branchStatus?.gitCommitHash}`
+                : "Rebase not available. Already up to date"
+            }
+          >
+            <IconButton
+              disabled={!canRebase || rebaseActive}
+              onClick={() =>
+                rebaseDraftMutation({
+                  draftId: draft.id,
+                  draftRebaseJob: {
+                    baseGitCommit: draft.baseGitCommit,
+                  },
+                })
+              }
+            >
+              <MergeIcon />
+            </IconButton>
+          </Tooltip>
+        }
+      </TableCell>
     </TableRow>
   );
 }
